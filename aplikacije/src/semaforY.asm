2c714b0987b0dd015ea3727c1b800906ba5e4d39
unknown <sashans13@hotmail.com>
2015-01-15 10:17:26 +0100

    Dodao vece semafore

--- aplikacije/src/semaforY.asm
+++ aplikacije/src/semaforY.asm
@@ -11,13 +11,57 @@
 
 %include "OS_API.inc"
 
-petlja:
+	call OS:_get_app_offset
+	mov  word [app_offset], ax
 
-	mov si, 1h
-	mov dh, 0
-	mov dl, 4Fh
-	mov di, 1
+	mov bh, 0h 							; pocetna iteracija
+
+petlja:
+	cmp bh, 6
+	jne .nastavi
+
+	mov bh, 0
+
+	.nastavi:
+
+	; prvi DL
+	mov si, LocDL
+	xor cx, cx
+	mov cl, bh
+
+	prvi:
+	inc si
+	loop prvi
+
+	mov dl, byte [si]
+
+	; drugi DH
+	mov si, LocDH
+	xor cx, cx
+	mov cl, bh
+
+	drugi:
+	inc si
+	loop drugi
+
+	mov dh, byte [si]
+
+	;treci DI
+	mov si, LocDI
+	xor cx, cx
+	mov cl, bh
+
+	treci:
+	inc si
+	loop treci
+
+	mov di, [si]
+	mov cx, di
+	and cx, 00FFh
+	mov di, cx
+
 	mov bl, 11101110b
+	mov si, 1h
 	push dx
 
 	cli									; Zabrani prekide. 
@@ -37,7 +81,11 @@ petlja:
 	out 070h, al						; Dozvoli prekide
 	sti
 
-
+	inc bh
 	jmp petlja
 
+LocDL db 4Ah, 4Bh, 4Ch, 4Dh, 4Eh, 4Fh
+LocDH db 02h, 01h, 00h, 00h, 01h, 02h
+LocDI db 03h, 04h, 05h, 05h, 04h, 03h
 
+app_offset dw 0
